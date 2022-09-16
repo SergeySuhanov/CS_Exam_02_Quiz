6a5f8c39d04d5dd474d82bcf7afe052dce893903
СергейС <beent@yandex.ru>
2022-09-16 20:02:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Exam_02_Quiz
+{
+    class User
+    {
+        string Name;
+        string Password;
+        DateTime BirthDate;
+    }
+
+    class Scoreboard
+    {
+        public string UserName;
+        int Score;
+    }
+
+    class Answer
+    {
+        string Text;
+        bool isCorrect;
+    }
+    class Question
+    {
+        string Discipline;
+        string QuizText;
+        List<Answer> Answers;
+    }
+
+    class Quiz
+    {
+        List<User> Users;
+        List<Scoreboard> Scoreboards;
+        List<Question> Questions;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
