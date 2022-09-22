a1a3562de8b6d07870ab5b6898139df90aa59b71
Сергей Суханов <beent@yandex.ru>
2022-09-22 21:14:43 +0300

    homework_22.09.2022

--- Program.cs
+++ Program.cs
@@ -41,12 +41,14 @@ namespace CS_Exam_02_Quiz
         public bool AskQuestion()
         {
             bool forNowIsCorrect = false;
-            Console.WriteLine(QuestionText);
+            Console.WriteLine($"\t{QuestionText}\n");
+
             for (int i = 0; i < AnswerText.Length; i++)
             {
                 Console.WriteLine($"{i + 1} - {AnswerText[i]}");
             }
             int choice = Int32.Parse(Console.ReadLine());
+
             for (int i = 0; i < IsCorrect.Length; i++)
             {
                 if (IsCorrect[i] == true)
@@ -216,157 +218,257 @@ namespace CS_Exam_02_Quiz
     {
         static void Main(string[] args)
         {
-            Quiz quiz = new Quiz();
-            quiz.LoadUsersList();
-            quiz.LoadScoreboard();
-            User player = new User();
-            int userIndex = -1;
-
-            bool inGame = true;
-            while (inGame)
+            bool in_menu_Lvl_1 = true;
+            while (in_menu_Lvl_1)
             {
-                
-                Console.WriteLine("\tВикторина");
-                Console.WriteLine("Войдите или Зарегистрируйтесь:\n");
-                Console.WriteLine("1 - Войти по лонину и паролю");
-                Console.WriteLine("2 - Регистрация нового пользователя");
-                int logreg = Int32.Parse(Console.ReadLine());
+                Quiz quiz = new Quiz();
+                quiz.LoadUsersList();
+                quiz.LoadScoreboard();
+                User player = new User();
+                int userIndex = -1;
 
-                switch (logreg)
+                bool in_menu_Lvl_2_1 = true;
+                while (in_menu_Lvl_2_1)
                 {
-                    case 1:
-                        string inputName;
-                        string inputPass;
-                        Console.WriteLine("Вход в систему\n");
-                        do
-                        {
-                            Console.Write("Введите логин: ");
-                            inputName = Console.ReadLine();
-                            userIndex = quiz.CheckUsersList(inputName);
-                        } while (userIndex < 0);
+                    Console.Clear();
+                    Console.WriteLine("\tВикторина");
+                    Console.WriteLine("Войдите или Зарегистрируйтесь:\n");
+                    Console.WriteLine("1 - Войти по лонину и паролю");
+                    Console.WriteLine("2 - Регистрация нового пользователя");
+                    int logreg = Int32.Parse(Console.ReadLine());
 
-                        player = quiz.Users[userIndex];
-
-                        do
-                        {
-                            Console.Write("Введите пароль: ");
-                            inputPass = Console.ReadLine();
-                        } while (player.Password != inputPass);
-                        Console.Write("пароль OK");
-                        Console.ReadLine();
-                        inGame = false;
-                        break;
-                    case 2:
-                        User newUser = new User();
-                        string newUserName;
-                        string newUserPassword;
-                        DateTime newUserBirthDay;
-                        Console.WriteLine("\tРегистрация\n");
-                        do
-                        {
-                            Console.Write("Введите логин: ");
-                            newUserName = Console.ReadLine();
-                            newUser.Name = newUserName;
-                        } while (quiz.CheckUsersList(newUserName) != -1);
-
-                        Console.Write("Введите пароль: ");
-                        newUserPassword = Console.ReadLine();
-                        newUser.Password = newUserPassword;
-
-                        Console.Write("Введите дату рождения: ");
-                        newUserBirthDay = DateTime.Parse(Console.ReadLine());
-                        newUser.BirthDate = newUserBirthDay;
-
-                        quiz.Users.Add(newUser);
-                        quiz.SaveUsersList();
-                        player = newUser;
-                        inGame = false;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            int score = 0;
-            Console.WriteLine("\tЭкран Игры");
-            Console.WriteLine("Выберите действие:\n");
-            Console.WriteLine("1 - Стартовать викторину");
-            Console.WriteLine("2 - Смотреть мои результаты");
-            Console.WriteLine("3 - Смотреть Топ-20 игроков");
-            Console.WriteLine("4 - Редактировать профиль");
-            int gameScreen = Int32.Parse(Console.ReadLine());
-
-            switch (gameScreen)
-            {
-                case 1:
-                    string[] questions = Directory.GetFiles($"{quiz.RootFolder}\\Questions");
-                    foreach (string file in questions)
-                    {
-                        quiz.LoadQuestion(file);
-                    }
-                    
-                    foreach (Question q in quiz.QuestionsPack)
-                    {
-                        if (q.AskQuestion())
-                            score++;
-                    }
-
-                    Score results = new Score();
-                    results.UserName = player.Name;
-                    results.Result = score;
-                    results.Discipline = "Mixed";
-                    results.Date = DateTime.Now;
-                    quiz.Scoreboard.Add(results);
-                    quiz.SaveScoreboard();
-                    break;
-                case 2:
-                    Console.WriteLine("\tМои лучшие результаты");
-                    Console.WriteLine("Score\t Discipline\t Date");
-                    foreach (Score entry in quiz.Scoreboard)
-                    {
-                        if (entry.UserName == player.Name)
-                        {
-                            Console.WriteLine($"{entry.Result}\t {entry.Discipline}\t\t {entry.Date}");
-                        }
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine("\tТоп-20 результатов");
-                    Console.WriteLine("Score\t Player\t Discipline\t Date");
-                    for (int i = 0; i < quiz.Scoreboard.Count; i++)
-                    {
-                        if (i < 20)
-                        {
-                            Console.WriteLine($"{quiz.Scoreboard[i].Result}\t {quiz.Scoreboard[i].UserName}\t {quiz.Scoreboard[i].Discipline}\t\t {quiz.Scoreboard[i].Date}");
-                        }
-                    }
-                    break;
-                case 4:
-                    Console.WriteLine("\tРедактор профиля");
-                    Console.WriteLine("Выберите действие:\n");
-                    Console.WriteLine("1 - Изменить пароль");
-                    Console.WriteLine("2 - Изменить дату рождения");
-                    int editChoice = Int32.Parse(Console.ReadLine());
-                    switch (editChoice)
+                    switch (logreg)
                     {
                         case 1:
-                            Console.Write("Введите новый пароль: ");
-                            player.Password = Console.ReadLine();
-                            Console.WriteLine("Пароль принят");
+                            string inputName;
+                            string inputPass;
+                            Console.WriteLine("Вход в систему\n");
+                            do
+                            {
+                                Console.Write("Введите логин: ");
+                                inputName = Console.ReadLine();
+                                userIndex = quiz.CheckUsersList(inputName);
+                            } while (userIndex < 0);
+
+                            player = quiz.Users[userIndex];
+
+                            do
+                            {
+                                Console.Write("Введите пароль: ");
+                                inputPass = Console.ReadLine();
+                            } while (player.Password != inputPass);
+                            Console.Write("пароль OK");
+                            Console.ReadLine();
+                            in_menu_Lvl_2_1 = false;
                             break;
                         case 2:
+                            User newUser = new User();
+                            string newUserName;
+                            string newUserPassword;
+                            DateTime newUserBirthDay;
+                            Console.WriteLine("\tРегистрация\n");
+                            do
+                            {
+                                Console.Write("Введите логин: ");
+                                newUserName = Console.ReadLine();
+                                newUser.Name = newUserName;
+                            } while (quiz.CheckUsersList(newUserName) != -1);
+
+                            Console.Write("Введите пароль: ");
+                            newUserPassword = Console.ReadLine();
+                            newUser.Password = newUserPassword;
+
                             Console.Write("Введите дату рождения: ");
-                            player.BirthDate = DateTime.Parse(Console.ReadLine());
-                            Console.WriteLine("Дата рождения изменена");
+                            newUserBirthDay = DateTime.Parse(Console.ReadLine());
+                            newUser.BirthDate = newUserBirthDay;
+
+                            quiz.Users.Add(newUser);
+                            quiz.SaveUsersList();
+                            player = newUser;
+                            in_menu_Lvl_2_1 = false;
                             break;
                         default:
                             break;
                     }
-                    quiz.Users[userIndex] = player;
-                    quiz.SaveUsersList();
-                    break;
-                default:
-                    break;
+                }
+
+                bool in_menu_Lvl_2_2 = true;
+                while (in_menu_Lvl_2_2)
+                {
+                    
+                    Console.Clear();
+                    Console.WriteLine("\tЭкран Игры");
+                    Console.WriteLine("Выберите действие:\n");
+                    Console.WriteLine("1 - Стартовать викторину");
+                    Console.WriteLine("2 - Смотреть мои результаты");
+                    Console.WriteLine("3 - Смотреть Топ-20 игроков");
+                    Console.WriteLine("4 - Редактировать профиль");
+                    Console.WriteLine("5 - Выход");
+                    int gameScreen = Int32.Parse(Console.ReadLine());
+
+                    switch (gameScreen)
+                    {
+                        case 1:
+                            string path = $"{quiz.RootFolder}\\Questions";
+                            string[] questions = Directory.GetFiles(path);
+                            string qFile;
+                            int score = 0;
+                            Score results = new Score();
+
+                            Console.Clear();
+                            Console.WriteLine("Выберите категорию:");
+                            Console.WriteLine("1 - История");
+                            Console.WriteLine("2 - География");
+                            Console.WriteLine("3 - Животные");
+                            Console.WriteLine("4 - Смешаная");
+                            int categChoice = Int32.Parse(Console.ReadLine());
+
+                            switch (categChoice)
+                            {
+                                case 1:
+                                    results.Discipline = "История";
+                                    foreach (string file in questions)
+                                    {
+                                        qFile = file.Substring(path.Length + 6);
+                                        if (qFile == "History.xml") { quiz.LoadQuestion(file); }
+                                    }
+                                    break;
+                                case 2:
+                                    results.Discipline = "География";
+                                    foreach (string file in questions)
+                                    {
+                                        qFile = file.Substring(path.Length + 6);
+                                        if (qFile == "Geography.xml") { quiz.LoadQuestion(file); }
+                                    }
+                                    break;
+                                case 3:
+                                    results.Discipline = "Животные";
+                                    foreach (string file in questions)
+                                    {
+                                        qFile = file.Substring(path.Length + 6);
+                                        if (qFile == "Animals.xml") { quiz.LoadQuestion(file); }
+                                    }
+                                    break;
+                                case 4:
+                                    results.Discipline = "Смешаная";
+                                    Random random = new Random();
+                                    List<string> qid = new List<string>();
+                                    bool isSameQuestion = false;
+
+                                    for (int i = 0; i < questions.Length; i++)
+                                    {
+                                        if (i < 20)
+                                        {
+                                            int qIndex = random.Next(0, questions.Length);
+                                            qFile = questions[qIndex].Substring(path.Length + 1, 4);
+                                            if (qid.Count == 0)
+                                            {
+                                                qid.Add(qFile);
+                                                quiz.LoadQuestion(questions[qIndex]);
+                                            }
+                                            else
+                                            {
+                                                isSameQuestion = false;
+                                                foreach (string id in qid)
+                                                {
+                                                    if (qFile == id) { isSameQuestion = true; }
+                                                }
+
+                                                if (!isSameQuestion)
+                                                {
+                                                    qid.Add(qFile);
+                                                    quiz.LoadQuestion(questions[qIndex]);
+                                                }
+                                            }
+                                        }  
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            foreach (Question q in quiz.QuestionsPack)
+                            {
+                                Console.Clear();
+                                bool right_answer = q.AskQuestion();
+                                if (right_answer == true) {
+                                    Console.WriteLine("Это правильный ответ!");
+                                    score++;
+                                } else {
+                                    Console.WriteLine("Ответ неверный!");
+                                }
+                                Console.ReadLine();
+                            }
+
+                            results.UserName = player.Name;
+                            results.Result = score;
+                            results.Date = DateTime.Now;
+                            quiz.Scoreboard.Add(results);
+                            quiz.SaveScoreboard();
+                            Console.WriteLine($"Игра окончена со счётом {score}");
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("\tМои лучшие результаты");
+                            Console.WriteLine("Score\t Discipline\t Date");
+                            foreach (Score entry in quiz.Scoreboard)
+                            {
+                                if (entry.UserName == player.Name)
+                                {
+                                    Console.WriteLine($"{entry.Result}\t {entry.Discipline}\t {entry.Date}");
+                                }
+                            }
+                            Console.WriteLine("\nнажмите любую клавишу...");
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("\tТоп-20 результатов");
+                            Console.WriteLine("Score\t Player\t Discipline\t Date");
+                            for (int i = 0; i < quiz.Scoreboard.Count; i++)
+                            {
+                                if (i < 20)
+                                {
+                                    Console.WriteLine($"{quiz.Scoreboard[i].Result}\t {quiz.Scoreboard[i].UserName}\t {quiz.Scoreboard[i].Discipline}\t {quiz.Scoreboard[i].Date}");
+                                }
+                            }
+                            Console.WriteLine("\nнажмите любую клавишу...");
+                            Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("\tРедактор профиля");
+                            Console.WriteLine("Выберите действие:\n");
+                            Console.WriteLine("1 - Изменить пароль");
+                            Console.WriteLine("2 - Изменить дату рождения");
+                            int editChoice = Int32.Parse(Console.ReadLine());
+                            switch (editChoice)
+                            {
+                                case 1:
+                                    Console.Write("Введите новый пароль: ");
+                                    player.Password = Console.ReadLine();
+                                    Console.WriteLine("Пароль принят");
+                                    Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.Write("Введите дату рождения: ");
+                                    player.BirthDate = DateTime.Parse(Console.ReadLine());
+                                    Console.WriteLine("Дата рождения изменена");
+                                    Console.ReadLine();
+                                    break;
+                                default:
+                                    break;
+                            }
+                            quiz.Users[userIndex] = player;
+                            quiz.SaveUsersList();
+                            break;
+                        case 5:
+                            in_menu_Lvl_2_2 = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
             
             Console.WriteLine("Program ended!");
